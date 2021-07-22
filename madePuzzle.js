6a698bb6dfa7329793e2c483f997d4afb2d71913
Park Na Yeon <within197@naver.com>
2021-07-22 23:10:45 +0900

    코드 리펙토링

--- madePuzzle.js
+++ madePuzzle.js
@@ -2,11 +2,9 @@ const controlBox = document.querySelector(".puzzle-control");
 const startBtn = controlBox.querySelector("button");
 const puzzleList = document.querySelectorAll(".puzzle-piece");
 
-const answer = [1, 2, 3, 4, 5, 6, 7, 8];
 let li = [];
 
 function madeQus() {
-  li = [];
   while (li.length < 8) {
     const n = Math.floor(Math.random() * 8) + 1;
     const check = li.indexOf(n);
@@ -23,8 +21,8 @@ function showPuzzle(p) {
   let x = 0;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      if (li[i][j] !== "") {
-        puzzleList[x].innerHTML = li[i][j];
+      if (p[i][j] !== "") {
+        puzzleList[x].innerHTML = p[i][j];
       }
       x += 1;
     }
